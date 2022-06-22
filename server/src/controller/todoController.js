e4105ad88b25b001ceca82529c43bd722d9aeeb5
Nurettin Şen <nurie487@gmail.com>
2022-06-22 20:47:10 +0300

    Update func set

--- server/src/controller/todoController.js
+++ server/src/controller/todoController.js
@@ -54,12 +54,24 @@ const deletebyId = async (req, res, next) => {
   }
 };
 
+const updatebyId = async (req, res, next) => {
+  try {
+    const todo = await Todo.findOne({
+      where: { id: req.body.editIndex },
+    });
+    if (!todo) {
+      return res.status(400).json({ error: "Opps Error" });
+    }
+    const updateData = await todo.update({ title: req.body.data });
+    return res.status(200).json(updateData);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
 module.exports = {
   create,
   getAll,
   deletebyId,
+  updatebyId,
 };
-
-//edit ve mark kaldı
-//geriye kalan postmande bakarak bakcend dokuman hazırla
-//
--- server/src/router/todoRouter.js
+++ server/src/router/todoRouter.js
@@ -5,5 +5,6 @@ const { isOpenSession } = require("./../middleweare/authMiddleweare");
 router.post("/todos", isOpenSession, todos.create);
 router.get("/todos/:id", isOpenSession, todos.getAll);
 router.delete("/todos/:todoId", isOpenSession, todos.deletebyId);
+router.post("/Updatetodo", isOpenSession, todos.updatebyId);
 
 module.exports = router;
